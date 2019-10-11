30984375e4c10e3664a8b45fe6ae2a51e3f42513
Neophear <stiiggade@gmail.com>
2019-10-11 10:42:22 +0200

    Initial commit

---
+++ SetVolume/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SetVolume
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSetVolume = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSetVolume
+            // 
+            this.btnSetVolume.Location = new System.Drawing.Point(12, 63);
+            this.btnSetVolume.Name = "btnSetVolume";
+            this.btnSetVolume.Size = new System.Drawing.Size(75, 23);
+            this.btnSetVolume.TabIndex = 0;
+            this.btnSetVolume.Text = "button1";
+            this.btnSetVolume.UseVisualStyleBackColor = true;
+            this.btnSetVolume.Click += new System.EventHandler(this.btnSetVolume_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 12);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnSetVolume);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSetVolume;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ SetVolume/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio;
+using NAudio.Mixer;
+using NAudio.Wave;
+
+namespace SetVolume
+{
+    public partial class Form1 : Form
+    {
+        WaveIn waveIn;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnSetVolume_Click(object sender, EventArgs e)
+        {
+            
+            int waveInDeviceNumber = waveIn.DeviceNumber;
+            
+            var mixerLine = new MixerLine((IntPtr)waveInDeviceNumber, 0, MixerFlags.WaveIn);
+            foreach (var control in mixerLine.Controls)
+            {
+                if (control.ControlType == MixerControlType.Volume)
+                {
+                    volumeControl = control as UnsignedMixerControl;
+                    MicrophoneLevel = desiredVolume;
+                    break;
+                }
+            }
+        }
+
+        public double MicrophoneLevel
+        {
+            get
+            {
+                return desiredVolume;
+            }
+            set
+            {
+                desiredVolume = value;
+                if (volumeControl != null)
+                {
+                    volumeControl.Percent = value;
+                }
+            }
+        }
+
+        public SampleAggregator SampleAggregator
+        {
+            get
+            {
+                return sampleAggregator;
+            }
+        }
+
+        public RecordingState RecordingState
+        {
+            get
+            {
+                return recordingState;
+            }
+        }
+
+        public TimeSpan RecordedTime
+        {
+            get
+            {
+                if (writer == null)
+                {
+                    return TimeSpan.Zero;
+                }
+                return TimeSpan.FromSeconds((double)writer.Length / writer.WaveFormat.AverageBytesPerSecond);
+            }
+        }
+
+        void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            byte[] buffer = e.Buffer;
+            int bytesRecorded = e.BytesRecorded;
+            WriteToFile(buffer, bytesRecorded);
+
+            for (int index = 0; index < e.BytesRecorded; index += 2)
+            {
+                short sample = (short)((buffer[index + 1] << 8) |
+                                        buffer[index + 0]);
+                float sample32 = sample / 32768f;
+                sampleAggregator.Add(sample32);
+            }
+        }
+
+        private void WriteToFile(byte[] buffer, int bytesRecorded)
+        {
+            long maxFileLength = this.recordingFormat.AverageBytesPerSecond * 60;
+
+            if (recordingState == RecordingState.Recording
+                || recordingState == RecordingState.RequestedStop)
+            {
+                var toWrite = (int)Math.Min(maxFileLength - writer.Length, bytesRecorded);
+                if (toWrite > 0)
+                {
+                    writer.Write(buffer, 0, bytesRecorded);
+                }
+                else
+                {
+                    Stop();
+                }
+            }
+        }
+    }
+}
